c52f9df9a625dee9487cd09727a7397486175f33
alumno <alumno@aula1-10>
2019-12-10 11:06:04 +0100

    Comentarios añadidos

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Ivan Torres
+ * @version 1.0
+ * Clase principal utilizando ramas
+ *
+ */
+public class Principal {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola ramas");
+
+	}
+
+}
